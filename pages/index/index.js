49619ae18134e24e383aa108caec041b2c4929c5
iceStone <ice@micua.com>
2016-12-05 10:40:58 +0800

    设计数据结构并完成页面数据绑定

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,16 @@
+// 1. 完成页面结构、布局、样式
+// 2. 设计数据结构
+// 3. 完成数据绑定
+// 4. 设计交互操作事件
+// 5. 数据存储
 Page({
+  data: {
+    todos: [
+      { name: 'Learning HTML5', completed: true },
+      { name: 'Learning CSS3', completed: false },
+      { name: 'Learning ECMAScript 2016', completed: false },
+      { name: 'Learning React', completed: false }
+    ],
+    leftCount: 3
+  }
 })
