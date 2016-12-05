aa6fa8dfcffd8596ba5caace238992d378b23091
iceStone <ice@micua.com>
2016-12-05 10:51:21 +0800

    添加任务逻辑

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,7 @@
 // 5. 数据存储
 Page({
   data: {
+    input: '',
     todos: [
       { name: 'Learning HTML5', completed: true },
       { name: 'Learning CSS3', completed: false },
@@ -12,5 +13,13 @@ Page({
       { name: 'Learning React', completed: false }
     ],
     leftCount: 3
+  },
+  inputChangeHandle: function (e) {
+    this.setData({ input: e.detail.value })
+  },
+  addTodoHandle: function (e) {
+    var todos = this.data.todos
+    todos.push({ name: this.data.input, completed: false })
+    this.setData({ todos: todos })
   }
 })
