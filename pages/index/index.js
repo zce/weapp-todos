bd96a6ef08a5ea2679eebe32dbafcb272c9e9154
zce <demo@itcast.cn>
2017-01-10 14:26:47 +0800

    logs

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,18 +9,24 @@ Page({
     input: '',
     todos: [],
     leftCount: 0,
-    allCompleted: false
+    allCompleted: false,
+    logs: []
   },
   save: function () {
-    wx.setStorageSync('todos_list', this.data.todos)
+    wx.setStorageSync('todo_list', this.data.todos)
+    wx.setStorageSync('todo_logs', this.data.logs)
   },
   load: function () {
-    var todos = wx.getStorageSync('todos_list')
+    var todos = wx.getStorageSync('todo_list')
     if (todos) {
       var leftCount = todos.filter(function (item) {
         return !item.completed
       }).length
       this.setData({ todos: todos, leftCount: leftCount })
+    }
+    var logs = wx.getStorageSync('todo_logs')
+    if (logs) {
+      this.setData({ logs: logs })
     }
   },
   // ===== 页面生命周期方法 =====
@@ -35,10 +41,13 @@ Page({
     if (!this.data.input || !this.data.input.trim()) return
     var todos = this.data.todos
     todos.push({ name: this.data.input, completed: false })
+    var logs = this.data.logs
+    logs.push({ timestamp: new Date(), action: '新增', name: this.data.input })
     this.setData({
       input: '',
       todos: todos,
-      leftCount: this.data.leftCount + 1
+      leftCount: this.data.leftCount + 1,
+      logs: logs
     })
     this.save()
   },
@@ -46,9 +55,16 @@ Page({
     var index = e.currentTarget.dataset.index
     var todos = this.data.todos
     todos[index].completed = !todos[index].completed
+    var logs = this.data.logs
+    logs.push({
+      timestamp: new Date(),
+      action: todos[index].completed ? '标记完成' : '标记未完成',
+      name: todos[index].name
+    })
     this.setData({
       todos: todos,
-      leftCount: this.data.leftCount + (todos[index].completed ? -1 : 1)
+      leftCount: this.data.leftCount + (todos[index].completed ? -1 : 1),
+      logs: logs
     })
     this.save()
   },
@@ -56,9 +72,12 @@ Page({
     var index = e.currentTarget.dataset.index
     var todos = this.data.todos
     var remove = todos.splice(index, 1)[0]
+    var logs = this.data.logs
+    logs.push({ timestamp: new Date(), action: '移除', name: remove.name })
     this.setData({
       todos: todos,
-      leftCount: this.data.leftCount - (remove.completed ? 0 : 1)
+      leftCount: this.data.leftCount - (remove.completed ? 0 : 1),
+      logs: logs
     })
     this.save()
   },
@@ -68,9 +87,16 @@ Page({
     for (var i = todos.length - 1; i >= 0; i--) {
       todos[i].completed = this.data.allCompleted
     }
+    var logs = this.data.logs
+    logs.push({
+      timestamp: new Date(),
+      action: this.data.allCompleted ? '标记完成' : '标记未完成',
+      name: '全部任务'
+    })
     this.setData({
       todos: todos,
-      leftCount: this.data.allCompleted ? 0 : todos.length
+      leftCount: this.data.allCompleted ? 0 : todos.length,
+      logs: logs
     })
     this.save()
   },
@@ -80,7 +106,13 @@ Page({
     for (var i = 0; i < todos.length; i++) {
       todos[i].completed || remains.push(todos[i])
     }
-    this.setData({ todos: remains })
+    var logs = this.data.logs
+    logs.push({
+      timestamp: new Date(),
+      action: '清空',
+      name: '已完成任务'
+    })
+    this.setData({ todos: remains, logs: logs })
     this.save()
   }
 })
